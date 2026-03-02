c45df15b8f4e17ceb9cdb211e0556531545e102f
Mohammed Hassan <mohammedhassanage26@gmail.com>
2026-03-02 17:26:00 +0100

    fix: disable stateful Sanctum as frontend uses Bearer tokens

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,activity-planning.167.235.58.232.sslip.io',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
